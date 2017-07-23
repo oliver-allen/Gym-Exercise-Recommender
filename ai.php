df88e95a25e4ec6190a6d2969a5260ea40de8b99
Oliver Allen <oliverallen96@gmail.com>
2017-07-24 00:39:21 +1200

    Starting AI. working on equipment filtering

--- ai.php
+++ ai.php
@@ -3,6 +3,60 @@
   //order of last done desending based on day only.
   //order dumbell/barbell used most (bilateral)
   //equipment type
-  echo "<tr><td>hi</td><td>hi</td><td>hi</td><td>hi</td></tr>";
 
+  require 'mysql_connection.php';
+  if (!$dbc) {
+    die("Connection failed: " . mysqli_connect_error());
+  }
+
+  $entries = mysqli_query($dbc, "SELECT * FROM $table ORDER BY lastDone ASC");
+  if(!$entries) {
+    die("Error select all. " . mysqli_error($dbc));
+  }
+
+  //filter based on equipment used over the last 7 days, recent has more weighting
+  $sql = mysqli_query($dbc, "SELECT equipment, lastDone FROM exercise WHERE lastDone >= NOW() - INTERVAL 7 DAY");
+  if(!$sql) {
+    die("Error select all. " . mysqli_error($dbc));
+  }
+  equipment($sql);
+
+
+
+
+  /*while ($row = mysqli_fetch_array($entries, MYSQLI_NUM)) {
+    echo "<tr>";
+    for($i=0; $i<count($row)-1; $i++){
+      echo "<td>" . $row[$i] . "</td>";
+    }
+    echo "</tr>";
+  }*/
+
+  mysqli_close($dbc);
+
+
+
+
+
+  function equipment($data){
+
+    $equipment = file("equipment.txt");
+    $array = array();
+    foreach ($equipment as $equip) {
+      $array[$equip] = 0;
+    }
+    $now = DateTime(date("Y-m-d H:i:s"));
+    echo "Time = $now now </br>";
+    while ($row = mysqli_fetch_assoc($data)) {
+      $date = $row['lastDone'];
+      echo $now->diff($date)->format('%a');
+    }
+
+
+    //Find difference between now and each entry. Multiply that equipment type by diff and add to array.
+
+    
+    //$array = mysqli_fetch_assoc($data);
+    //print_r($array);
+  }
  ?>
--- mysql_connection.php
+++ mysql_connection.php
@@ -1,5 +1,9 @@
 <?php
 
+if(!date_default_timezone_set('NZ')){
+  die("Error setting defualt time zone");
+}
+
 $dbc = @mysqli_connect('localhost', 'root', 'root', 'exercises')
 OR die('Could not connect to MySQL ' . mysqli_connect_error());
 $table = "exercise";
