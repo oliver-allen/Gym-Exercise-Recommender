2836d146d7d756710b90082f9a79758f3a87245b
Oliver Allen <oliverallen96@gmail.com>
2019-05-14 22:17:30 +1200

    Create exercise table dump.sql

---
+++ exercise table dump.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Oct 31, 2017 at 11:37 AM
+-- Server version: 5.6.34
+-- PHP Version: 7.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+--
+-- Dumping data for table `exercise`
+--
+
+INSERT INTO `q` (`name`, `primaryPart`, `secondaryPart`, `equipment`, `bilateral`, `lastDone`) VALUES
+('a', 'Chest', 'None', 'Dumbell', 0, '2017-10-31 16:37:36'),
+('Bench Press', 'Chest', 'Tricep', 'Dumbell', 1, '2017-10-31 22:23:09'),
+('Bicep Barbell Curl', 'Bicep', 'None', 'Barbell', 1, '2017-10-29 23:08:27'),
+('Bicep Dumbell Curl', 'Bicep', 'None', 'Dumbell', 0, '2017-07-23 13:01:53'),
+('bilat', 'Chest', 'None', 'Dumbell', 0, '2017-10-31 16:33:12'),
+('bilateral', 'Tricep', 'None', 'Dumbell', 1, '2017-10-31 16:38:00'),
+('Dumbell Bench Press', 'Chest', 'Tricep', 'Dumbell', 1, '2017-10-31 23:33:28'),
+('ewer', 'Bicep', 'None', 'Dumbell', 0, '2017-10-29 19:54:23'),
+('fed', 'Chest', 'Chest', 'Dumbell', 0, '2017-10-29 22:30:16'),
+('ff', 'Chest', 'Tricep', 'Barbell', 1, '2017-10-30 23:17:01'),
+('hi', 'Bicep', 'Chest', 'Dumbell', 0, '2017-10-29 20:29:57'),
+('last', 'Bicep', 'None', 'Barbell', 1, '2017-07-24 00:11:00'),
+('me', 'Bicep', 'None', 'Dumbell', 1, '2017-07-23 00:16:35'),
+('plate', 'Bicep', 'None', 'Plate', 0, '2017-10-31 00:14:29'),
+('qwe', 'Bicep', 'Chest', 'Dumbell', 1, '2017-07-23 13:00:27'),
+('unilat', 'Chest', 'None', 'Cable', 0, '2017-10-31 16:35:39'),
+('ve', 'Chest', 'None', 'Dumbell', 1, '2017-07-23 00:03:11'),
+('ver', 'Chest', 'None', 'Dumbell', 0, '2017-07-23 00:03:37'),
+('very', 'Chest', 'None', 'Dumbell', 0, '2017-07-23 23:20:30'),
+('warty', 'Bicep', 'Chest', 'Dumbell', 0, '2017-07-23 00:18:41'),
+('wartyy', 'Bicep', 'Chest', 'Dumbell', 0, '2017-07-23 00:19:11'),
+('we', 'Chest', 'None', 'Dumbell', 0, '2017-10-29 22:41:56'),
+('wert', 'Bicep', 'Chest', 'Dumbell', 0, '2017-07-23 00:17:51');
